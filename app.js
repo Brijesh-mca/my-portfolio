57ae9e0f43512d115096e5229beb28dc4302b6be
Brijesh-mca <ibrijesh.mca@gmail.com>
2024-08-14 09:22:07 -0500

    files Uploaded

---
+++ app.js
@@ -0,0 +1,40 @@
+gsap.from("#ani1",{
+    x:-600,
+    duration:1,
+    delay:1
+});
+
+gsap.from("#ani2",{
+    x:600,
+    duration:2,
+    delay:1
+})
+
+gsap.from("#Services",{
+    scale:0,
+    duration:1,
+    delay:1,
+    scrollTrigger:"#Services"
+})
+
+gsap.from("#experience",{
+    scale:0,
+    duration:1,
+    delay:0.6,
+    scrollTrigger:"#experience"
+})
+
+gsap.from("#Education",{
+    scale:0,
+    duration:1,
+    delay:0.6,
+    scrollTrigger:"#Education"
+})
+
+gsap.from("#Skills",{
+    scale:0,
+    duration:1,
+    delay:0.6,
+    scrollTrigger:"#Skills"
+})
+
