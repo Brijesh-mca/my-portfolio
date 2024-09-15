b1e625c10cf6aca8d1664fb0424a89d17a4e6f29
Brijesh-mca <ibrijesh.mca@gmail.com>
2024-09-14 20:27:12 -0500

    Styles changes

--- app.js
+++ app.js
@@ -1,81 +1,81 @@
 gsap.from("#ani1",{
     x:-600,
-    duration:1,
+    duration:0.5,
     delay:0.5
 });
 
 gsap.from("#ani2",{
     x:600,
-    duration:2,
+    duration:0.5,
     delay:0.5
 })
 
 gsap.from("#Services",{
     scale:0,
-    duration:1,
+    duration:0.5,
     delay:0.5,
     scrollTrigger:"#Services"
 })
 
 gsap.from("#experience",{
     scale:0,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#experience"
 })
 
 gsap.from("#Education",{
     scale:0,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#Education"
 })
 
 gsap.from("#Skills",{
     scale:0,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#Skills"
 })
 gsap.from("#one",{
     scale:0,
     x:-200,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#one"
 })
 gsap.from("#two",{
     scale:0,
     y:100,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#two"
 })
 gsap.from("#three",{
     scale:0,
     x:200,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#three"
 })
 gsap.from("#four",{
     scale:0,
     x:200,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#four"
 })
 gsap.from("#five",{
     scale:0,
     y:-100,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#five"
 })
 gsap.from("#six",{
     scale:0,
     x:-200,
-    duration:1,
+    duration:0.5,
     delay:0.6,
     scrollTrigger:"#six"
 })
